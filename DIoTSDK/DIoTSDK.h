788ed7d69df4d1eaf91f1a4395cb7a6add0ff6f2
Andrey <rtcveel@gmail.com>
2022-06-24 21:20:59 +0400

    Initial Commit

---
+++ DIoTSDK/DIoTSDK.h
@@ -0,0 +1,18 @@
+//
+//  DIoTSDK.h
+//  DIoTSDK
+//
+//  Created by Andrey Maslennikov on 24.06.2022.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for DIoTSDK.
+FOUNDATION_EXPORT double DIoTSDKVersionNumber;
+
+//! Project version string for DIoTSDK.
+FOUNDATION_EXPORT const unsigned char DIoTSDKVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <DIoTSDK/PublicHeader.h>
+
+
